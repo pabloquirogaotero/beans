671b53a1fc2a20075c5a4dda7fd5b38992091e3d
Pablo Quiroga Otero <295206050+pabloquirogaotero@users.noreply.github.com>
2026-09-13 19:46:19 +0200

    feat: map ordering exceptions to HTTP responses

---
+++ src/main/java/com/quirogaotero/beans/ordering/adapter/in/web/OrderingExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.quirogaotero.beans.ordering.adapter.in.web;
+
+import com.quirogaotero.beans.ordering.application.CheckoutNotFoundException;
+import com.quirogaotero.beans.ordering.application.PaymentDeclinedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class OrderingExceptionHandler {
+
+    @ExceptionHandler(CheckoutNotFoundException.class)
+    ProblemDetail handleNotFound(CheckoutNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(PaymentDeclinedException.class)
+    ProblemDetail handleDeclined(PaymentDeclinedException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.PAYMENT_REQUIRED, ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ProblemDetail handleConflict(IllegalStateException ex) {
+        // e.g. paying an already-paid checkout
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+}
