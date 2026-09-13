270ea1bc792426347a560b6f89134b4ba68368d6
Pablo Quiroga Otero <295206050+pabloquirogaotero@users.noreply.github.com>
2026-09-13 18:02:21 +0200

    feat: persist a Checkout when starting one

---
+++ src/test/java/com/quirogaotero/beans/ordering/application/port/out/InMemoryCheckoutRepository.java
@@ -0,0 +1,18 @@
+package com.quirogaotero.beans.ordering.application.port.out;
+
+import com.quirogaotero.beans.inventory.domain.CheckoutId;
+import com.quirogaotero.beans.ordering.domain.Checkout;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+class InMemoryCheckoutRepository implements CheckoutRepository {
+
+    final Map<CheckoutId, Checkout> store = new HashMap<>();
+
+    public void save(Checkout checkout) { store.put(checkout.getId(), checkout); }
+
+    public Optional<Checkout> findById(CheckoutId id) { return Optional.ofNullable(store.get(id)); }
+
+}
